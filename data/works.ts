377820294bea35c46abf0b8107951800f17bdce6
小智 <xiaozhi@192.168.1.9>
2026-05-18 20:40:58 +0800

    Fix B站 embed: protocol-relative URL, high_quality and as_wide params

--- data/works.ts
+++ data/works.ts
@@ -12,7 +12,7 @@ export type Work = {
 };
 
 export function getBilibiliEmbed(bvid: string) {
-  return `https://player.bilibili.com/player.html?bvid=${bvid}&page=1&autoplay=0`;
+  return `//player.bilibili.com/player.html?bvid=${bvid}&page=1&high_quality=1&as_wide=1`;
 }
 
 export function getVideoThumbnail(bvid: string) {
